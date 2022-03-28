8f13caf1346d4d6375348ccfa56fabb6b37abf3d
Dilraj Bal <dilraj.bal@hotmail.com>
2022-03-27 20:59:03 -0700

    Added grayscale and sepia functions

--- helper.c
+++ helper.c
@@ -1,14 +1,58 @@
 #include "helpers.h"
-
+#include <math.h>
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int avg = 0;
+    for (int i = 0; i < height; i ++){
+        for (int j = 0; j < width; j++){
+            avg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3;
+            if (avg > 127) {
+                image[i][j].rgbtBlue = 255;
+                image[i][j].rgbtGreen = 255;
+                image[i][j].rgbtRed = 255;
+            }
+            else{
+                image[i][j].rgbtBlue = 0;
+                image[i][j].rgbtGreen = 0;
+                image[i][j].rgbtRed = 0;
+
+            }
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed = 0;
+    int sepiaBlue = 0;
+    int sepiaGreen = 0;
+    
+    for (int i = 0; i < height; i ++){
+        for (int j = 0; j < width; j++){
+            sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
+            if (sepiaRed > 255){
+                sepiaRed = 255;
+            }
+
+            if (sepiaGreen > 255){
+                sepiaGreen = 255;
+            }
+
+            if (sepiaBlue > 255){
+                sepiaBlue = 255;
+            }
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
